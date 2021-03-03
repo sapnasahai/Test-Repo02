c35364c8751c97908abe6de2a058fa2420fa2a87
sapnasahai <47326414+sapnasahai@users.noreply.github.com>
2021-03-03 17:49:52 +0530

    particle effect

---
+++ GTA/Assets/DustEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustEffect : MonoBehaviour
+{
+    public ParticleSystem dust;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Createdust();
+            transform.position += new Vector3(-1, 0, 0); 
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Createdust();
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+
+
+    void Createdust()
+    {
+        dust.Play();
+    }
+}
---
+++ GTA/Assets/dust.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dust : MonoBehaviour
+{
+
+
+    public ParticleSystem Dust;
+
+
+
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            CreateDust();
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            CreateDust();
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+
+    }
+        // Update is called once per frame
+    void CreateDust()
+    {
+            Dust.Play();
+    }
+
+    
+}
